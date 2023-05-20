42b375514858457615dc162ecaf3a4d787c4a287
hharieta <L20530228@cancun.tecnm.mx>
2023-05-20 04:13:33 -0500

    AuthPage function added

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:macha/constants/navigation.dart';
+import 'package:macha/auth/auth.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -13,7 +13,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Navi(),
+      // home: Navi(),
+      home: AuthPage(),
     );
   }
 }
